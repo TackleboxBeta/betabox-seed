3a94837a6411c65b99ca694c6e99effa25e35cbd
Trevor Ewen <tewen@Trevors-MacBook-Pro-3.local>
2017-07-22 15:15:14 -0400

    Mailer example up with better scaffolding.

--- scaffolding/util.js
+++ scaffolding/util.js
@@ -3,9 +3,10 @@ const fs = require('fs-extra');
 
 exports.writeToEnvFile = (filename, key, value) => {
   const env = _.chain(fs.readFileSync(filename, 'utf-8').split('\n')).reduce((acc, row) => {
-    const { k, v } = row.split('=');
+    const [k, v] = row.split('=');
     acc[k] = v;
     return acc;
-  }, {}).reject(_.isNil).value();
-  fs.writeFileSync(filename, _.reduce(_.merge({}, env, { [key]: value }), (acc, v, k) => acc.concat(`${k}=${v}`), []).join('\n'));
+  }, {}).omitBy(_.isNil).value();
+  fs.writeFileSync(filename,
+    _.reduce(_.merge({}, env, { [key]: value }), (acc, v, k) => acc.concat(`${k}=${v}`), []).join('\n'));
 };
