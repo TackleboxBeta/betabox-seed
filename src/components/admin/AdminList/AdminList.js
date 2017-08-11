7e7f3bc0c3fd4fa1ea5ad4d3d4e40740be801085
Trevor Ewen <tewen@Trevors-MacBook-Pro-3.local>
2017-08-10 22:42:21 -0400

    Working through the new AdminList component.

---
+++ src/components/admin/AdminList/AdminList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+
+function th({ name }) {
+  return (
+    <th>{name}</th>
+  );
+}
+
+function tr(columns, data) {
+  
+}
+
+export default class AdminList extends Component {
+  static propTypes = {
+    columns: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string, key: PropTypes.string })),
+    data: PropTypes.arrayOf(PropTypes.object)
+  };
+
+  render() {
+    const { columns, data } = this.props;
+    return (
+      <table className="table table-hover">
+        <thead>{_.map(columns, th)}</thead>
+        <tbody>{_.map(data, _.partial(tr, columns))}</tbody>
+      </table>
+    );
+  }
+}
